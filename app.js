c52eeb60aed7e6d7d697d8a2c3c23a235ae91845
Samantha Klein <saradurga@gmail.com>
2016-05-04 08:48:40 -0500

    final step + more colors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-var colors = ["blue", "green", "gold", "indigo"];
+var colors = ["blue", "green", "gold", "indigo", "peachpuff", "firebrick"];
 //Append boxes with div and data-id
 
-  $('body').append('<div class="blue box" data-id="0"></div>');
-  $('body').append('<div class="green box" data-id="1"></div>');
-  $('body').append('<div class="gold box" data-id="2"></div>');
-  $('body').append('<div class="indigo box" data-id="3"></div>');
+  $('body').append('<div class="blue box"></div>');
+  $('body').append('<div class="green box"></div>');
+  $('body').append('<div class="gold box"></div>');
+  $('body').append('<div class="indigo box"></div>');
+  $('body').append('<div class="peachpuff box"></div>');
+  $('body').append('<div class="firebrick box"></div>');
 
 chooseBlock();
 //Event listener for click
@@ -13,7 +15,9 @@ $('body').on('click', '.box', function () {
   if ($(this).hasClass('correct')) {
     alert("You win!");
     $('.correct').removeClass('correct');
+    $('.prompt').remove();
     chooseBlock();
+
 
 } else {
   alert("Nope.");
@@ -28,7 +32,8 @@ function randomNumber(min, max){
 }
 
 function chooseBlock () {
-var number = randomNumber(0, 3);
+var number = randomNumber(0, 5);
 $('.' + colors[number]).addClass('correct');
+$('body').append('<p class="prompt">Click on ' + colors[number] + '!</p>');
 }
 });
